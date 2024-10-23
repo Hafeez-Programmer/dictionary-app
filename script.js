9fcbfc837f340c2062e5203e961c6d6c6e9ef63c
Hafeez ur rehman <hr3181664924@gmail.com>
2024-10-23 16:30:47 +0500

    error handling

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const resultContainerElement = document.querySelector("#result-container");
 const inputELement = document.querySelector("#input");
 const searchBtnElement = document.querySelector("#search-btn");
 
-
 searchBtnElement.addEventListener('click', () => {
   getdata();
 })
@@ -14,51 +13,60 @@ inputELement.addEventListener('keypress', (e) => {
 })
 
 const getdata = async () => {
-    let word = inputELement.value;
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  
+  let word = inputELement.value;
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  try {
     let response = await fetch(url);
-    let data = await response.json();
-    console.log(data[0]);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
     
+    let data = await response.json();
+    
+    console.log(data[0].meanings);
+
     displayResult(word);
+  } catch (error) {
+    console.error("Fetch error:", error.message);
+  }
 };
 
 function displayResult(word) {
     resultContainerElement.innerHTML = `
-                <div class="row">
-  
-                <div class="col-10 mb-3">
-                  <h4>${word}</h4>
-                </div>
-                <div class="col-2">
-                  <button class="btn btn-outline-dark">
-                    <i class="bi bi-volume-up fs-5"></i>
-                  </button>
-                </div>
-  
-                <hr>
-  
-                <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
-                  <h4>Meaning</h4>
-                  <p>this is meaning</p>
-                </div>
-  
-                <hr>
-  
-                <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
-                  <h4>Example</h4>
-                  <p>this is example</p>
-                </div>
-  
-                <hr>
-  
-                <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
-                  <h4>Synonyms</h4>
-                  <p>this is Synonyms</p>
-                </div>
-  
-              </div>            
+      <div class="row">
+
+      <div class="col-10 mb-3">
+        <h4>${word}</h4>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-outline-dark">
+          <i class="bi bi-volume-up fs-5"></i>
+        </button>
+      </div>
+
+      <hr>
+
+      <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
+        <h4>Meaning</h4>
+        <p>this is meaning</p>
+      </div>
+
+      <hr>
+
+      <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
+        <h4>Example</h4>
+        <p>this is example</p>
+      </div>
+
+      <hr>
+
+      <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
+        <h4>Synonyms</h4>
+        <p>this is Synonyms</p>
+      </div>
+
+    </div>            
   `
 }
 
