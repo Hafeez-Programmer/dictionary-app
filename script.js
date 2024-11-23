d5a6a751c914545b2ee56322d97809c0cd6d74c3
Hafeez ur rehman <hr3181664924@gmail.com>
2024-11-23 18:55:42 +0500

    meanings, synonyms and antonyms completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const resultContainerElement = document.querySelector("#result-container");
-const inputELement = document.querySelector("#input");
-const searchBtnElement = document.querySelector("#search-btn");
+let resultContainerElement = document.querySelector("#result-container");
+let inputELement = document.querySelector("#input");
+let searchBtnElement = document.querySelector("#search-btn");
 
 searchBtnElement.addEventListener('click', () => {
   getdata();
@@ -12,10 +12,11 @@ inputELement.addEventListener('keypress', (e) => {
   }
 })
 
+
 const getdata = async () => {
   let word = inputELement.value;
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-
+  
   try {
     let response = await fetch(url);
     if (!response.ok) {
@@ -24,50 +25,98 @@ const getdata = async () => {
     
     let data = await response.json();
     
-    console.log(data[0].meanings);
-
-    displayResult(word);
+    // console.log(data[0]);
+    
+    displayResult(word, data);
+    
   } catch (error) {
     console.error("Fetch error:", error.message);
   }
 };
 
-function displayResult(word) {
-    resultContainerElement.innerHTML = `
-      <div class="row">
+function displayResult(word, data) {
+  resultContainerElement.innerHTML = `
+    <div class="row">
+    
+    <div class="col-10 mb-3">
+      <h4>${word}</h4>
+    </div>
+    <div class="col-2">
+      <button class="btn btn-outline-dark">
+      <i class="bi bi-volume-up fs-5"></i>
+      </button>
+    </div>
+    
+    <hr>
 
-      <div class="col-10 mb-3">
-        <h4>${word}</h4>
+    <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
+    <h4>Meanings</h4>
+    <div id="meaningContainer" class="row">
+    
+    
+    </div>
+    </div>
+    
+    <hr>
+    
+    <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
+    <h4>Antonyms</h4>
+    <div class="row">
+    
+    <div id="antonymsContainer" class="col-12">
+    </div>
+    
+    </div>
+    </div>
+    
+    <hr>
+    
+    <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
+    <h4>Synonyms</h4>
+    <div class="row">
+    
+      <div id="synonymsContainer" class="col-12">
       </div>
-      <div class="col-2">
-        <button class="btn btn-outline-dark">
-          <i class="bi bi-volume-up fs-5"></i>
-        </button>
+      
+    </div>
       </div>
-
-      <hr>
-
-      <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
-        <h4>Meaning</h4>
-        <p>this is meaning</p>
-      </div>
-
-      <hr>
-
-      <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
-        <h4>Example</h4>
-        <p>this is example</p>
-      </div>
-
-      <hr>
-
-      <div class="col-12 mb-3 border-start border-5 rounded-1 border-dark">
-        <h4>Synonyms</h4>
-        <p>this is Synonyms</p>
-      </div>
-
+      
     </div>            
-  `
+    `
+  let meaningContainerElement = document.querySelector("#meaningContainer");
+  let antonymsContainerElement = document.querySelector("#antonymsContainer")
+  let synonymsContainerElement = document.querySelector("#synonymsContainer");
+  
+  
+  data[0].meanings.forEach(meaning_object => {
+    // meanings 
+    let partOfSpeech = meaning_object.partOfSpeech;
+    let definition = meaning_object.definitions[0].definition;  
+    
+    // antonyms
+    meaning_object.antonyms.forEach(antonym_object => {
+      antonymsContainerElement.innerHTML += `
+        <p class="d-inline">${antonym_object},</p>
+      `      
+    });
+
+    meaning_object.synonyms.forEach(synonym_object => {
+      synonymsContainerElement.innerHTML += `
+        <p class="d-inline">${synonym_object},</p>
+      `      
+    });
+
+
+    
+    
+    meaningContainerElement.innerHTML += `
+      <div class="col-12">      
+        <h6 class="d-inline">${partOfSpeech}:</h6>
+        <p class="d-inline">${definition}</p>
+      </div>        
+      `
+    
+    });      
 }
 
 
